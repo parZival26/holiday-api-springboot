6aa6da54815eb2be72f89fab5a6dc7d503736e6b
Juan José Medina <jjr20060326@gmail.com>
2025-04-08 11:37:10 -0500

    init

---
+++ src/main/java/com/parZival26/holiday_api_springboot/HolidayApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.parZival26.holiday_api_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayApiSpringbootApplication.class, args);
+	}
+
+}
